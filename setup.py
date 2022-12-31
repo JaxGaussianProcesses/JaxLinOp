08a7d6b9ec06353954d645d70c1a9f0b02ff38dc
Daniel Dodd <daniel_dodd@icloud.com>
2022-12-31 15:44:02 +0000

    Incorporate versioneer.

--- setup.py
+++ setup.py
@@ -1,12 +1,30 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import versioneer
 import codecs
 import os
 import re
 from setuptools import find_packages, setup
 
 NAME = "jaxlinop"
+
+# Handle builds of nightly release - adapted from BlackJax.
+if "BUILD_PACKAGENAME_NIGHTLY" in os.environ:
+    if os.environ["BUILD_PACKAGENAME_NIGHTLY"] == "nightly":
+        NAME += "-nightly"
+
+        from versioneer import get_versions as original_get_versions
+
+        def get_versions():
+            from datetime import datetime, timezone
+
+            suffix = datetime.now(timezone.utc).strftime(r".dev%Y%m%d")
+            versions = original_get_versions()
+            versions["version"] = versions["version"].split("+")[0] + suffix
+            return versions
+
+        versioneer.get_versions = get_versions
 
 CLASSIFIERS = [
     "Intended Audience :: Science/Research",
@@ -73,7 +91,8 @@ def find_meta(meta: str) -> str:
 if __name__ == "__main__":
     setup(
         name=NAME,
-        version=find_meta("version"),
+        version=versioneer.get_version(),
+        cmdclass=versioneer.get_cmdclass(),
         author=find_meta("authors"),
         author_email=find_meta("emails"),
         maintainer=find_meta("authors"),
